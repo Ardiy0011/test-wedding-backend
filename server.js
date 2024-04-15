589cdac999fc03c37c00979ede83e28ee3303668
Ardiy0011 <ardiyquarsh@gmail.com>
2024-04-15 01:18:28 -0700

    buter keeten

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Set up CORS
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3001', 'https://joseph-x-geraldine.we-devsgh.online/dashboard/'],
+    origin: ['*'],
     credentials: true,
 }));
 
